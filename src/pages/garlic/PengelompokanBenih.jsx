d27fea7ca4af9dceef74f676141e6f551109c095
NovaBam123 <ibam.nva@gmail.com>
2025-05-31 10:37:05 +0700

    remove emoji from seed topic

--- src/pages/garlic/PengelompokanBenih.jsx
+++ src/pages/garlic/PengelompokanBenih.jsx
@@ -7,31 +7,31 @@ function PengelempokanBenih() {
             <h4>Pengelompokan Jenis Benih/Umbi dalam Sistem Perbenihan Nasional</h4>
              <p>🟡<strong> Benih Penjenis(Breeder Seed)</strong> (Label Kuning)</p>
             <ul>
-                <li>🧬 Tahap awal benih yang merupakan hasil seleksi dari varietas unggul yang dilepas secara resmi juga memiliki kemurnian genetik paling tinggi dan di produksi terbatas.</li>
-                <li>🧑‍🔬 Diproduksi oleh: Pemulia atau institusi resmi pemegang hak varietas.</li>
-                <li>📦 Penggunaan: Merupakan benih sumber utama dalam seluruh sistem produksi benih.</li>
+                <li> Tahap awal benih yang merupakan hasil seleksi dari varietas unggul yang dilepas secara resmi juga memiliki kemurnian genetik paling tinggi dan di produksi terbatas.</li>
+                <li> Diproduksi oleh: Pemulia atau institusi resmi pemegang hak varietas.</li>
+                <li> Penggunaan: Merupakan benih sumber utama dalam seluruh sistem produksi benih.</li>
             </ul>
             <p>⚪ <strong>Benih Dasar(Foundation Seed)</strong> (Label Putih)</p>
             <ul>
-                <li>✅ Definisi: Benih hasil perbanyakan dari benih penjenis.</li>
-                <li>🧬 Tujuan: Menjaga stabilitas sifat genetik varietas unggul.</li>
-                <li>🧑‍🔬 Diproduksi oleh: Institusi pemuliaan (balai penelitian, perguruan tinggi, atau perusahaan resmi).</li>
-                <li>📦 Penggunaan: Tidak langsung ditanam petani; tapi digunakan oleh instansti terkait/balai untuk memproduksi Benih Pokok.</li>
-                <li>🎯 Pentingnya: Menjadi “sumber induk” bagi seluruh sistem perbenihan, menjaga kualitas varietas unggul.</li>
+                <li> Definisi: Benih hasil perbanyakan dari benih penjenis.</li>
+                <li> Tujuan: Menjaga stabilitas sifat genetik varietas unggul.</li>
+                <li> Diproduksi oleh: Institusi pemuliaan (balai penelitian, perguruan tinggi, atau perusahaan resmi).</li>
+                <li> Penggunaan: Tidak langsung ditanam petani; tapi digunakan oleh instansti terkait/balai untuk memproduksi Benih Pokok.</li>
+                <li> Pentingnya: Menjadi “sumber induk” bagi seluruh sistem perbenihan, menjaga kualitas varietas unggul.</li>
             </ul>
             <p>🟣 <strong>Benih Pokok(Stock Seed)</strong> (Label Ungu)</p>
             <ul>
-                <li>✅ Definisi: Benih hasil perbanyakan dari benih dasar, tetap menjaga kemurnian genetik tinggi.</li>
-                <li>🧑‍🔬 Diproduksi oleh: Lembaga yang ditunjuk (balai benih, unit produksi benih bersertifikat).</li>
-                <li>📦 Penggunaan: Untuk memproduksi Benih Sebar.</li>
-                <li>🎯 Pentingnya: Menjembatani antara benih dasar dan benih sebar, memperluas cakupan kegunaan benih dengan tetap menjaga standar mutu.</li>
+                <li> Definisi: Benih hasil perbanyakan dari benih dasar, tetap menjaga kemurnian genetik tinggi.</li>
+                <li> Diproduksi oleh: Lembaga yang ditunjuk (balai benih, unit produksi benih bersertifikat).</li>
+                <li> Penggunaan: Untuk memproduksi Benih Sebar.</li>
+                <li> Pentingnya: Menjembatani antara benih dasar dan benih sebar, memperluas cakupan kegunaan benih dengan tetap menjaga standar mutu.</li>
             </ul>
             <p> 🔵<strong>Benih Sebar(Extension Seed)</strong> (Label Biru)</p>
             <ul>
-                <li>✅ Definisi: Benih hasil perbanyakan dari benih dasar untuk disebarluaskan dan digunakan oleh petani secara langsung.</li>
-                <li>👨‍🌾 Diproduksi oleh: Petani penangkar atau produsen benih resmi.</li>
-                <li>🌾 Penggunaan: Untuk budidaya skala produksi, digunakan langsung di lahan komersil.</li>
-                <li>🎯 Pentingnya: Menjadi benih konsumsi akhir yang menjangkau petani, menentukan dan mempertahankan sifat2 unggul suatu varietas juga hasil panen di tingkat lapangan.</li>
+                <li> Definisi: Benih hasil perbanyakan dari benih dasar untuk disebarluaskan dan digunakan oleh petani secara langsung.</li>
+                <li> Diproduksi oleh: Petani penangkar atau produsen benih resmi.</li>
+                <li> Penggunaan: Untuk budidaya skala produksi, digunakan langsung di lahan komersil.</li>
+                <li> Pentingnya: Menjadi benih konsumsi akhir yang menjangkau petani, menentukan dan mempertahankan sifat2 unggul suatu varietas juga hasil panen di tingkat lapangan.</li>
             </ul>
             <h4>Alur Produksi Benih menurut Sistem Nasional</h4>
             <p> → 🟡 Benih Penjenis (Breeder Seed)</p>
